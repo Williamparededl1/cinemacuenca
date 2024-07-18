95f7c00e2c366846d08c5d53dad8c90c7f52709f
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-18 16:44:23 -0500

    mapper de MovieMovieDb para el Movie entity

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,28 @@
+import 'package:cinemacuenca/domain/entities/movie.dart';
+import 'package:cinemacuenca/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDb movieDBToMovieDB) => Movie(
+      adult: movieDBToMovieDB.adult,
+      backdropPath: (movieDBToMovieDB.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDBToMovieDB.backdropPath}'
+          : 'https://linnea.com.ar/wp-content/uploads/2018/09/404PosterNotFound.jpg',
+      genreIds: movieDBToMovieDB.genreIds
+          .map(
+            (e) => e.toString(),
+          )
+          .toList(),
+      id: movieDBToMovieDB.id,
+      originalLanguage: movieDBToMovieDB.originalLanguage,
+      originalTitle: movieDBToMovieDB.originalTitle,
+      overview: movieDBToMovieDB.overview,
+      popularity: movieDBToMovieDB.popularity,
+      posterPath: (movieDBToMovieDB.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movieDBToMovieDB.posterPath}'
+          : 'https://linnea.com.ar/wp-content/uploads/2018/09/404PosterNotFound.jpg',
+      releaseDate: movieDBToMovieDB.releaseDate,
+      title: movieDBToMovieDB.title,
+      video: movieDBToMovieDB.video,
+      voteAverage: movieDBToMovieDB.voteAverage,
+      voteCount: movieDBToMovieDB.voteCount);
+}
