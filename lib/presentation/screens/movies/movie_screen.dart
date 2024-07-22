663356ac4a7d6e4a21235789ee14a6adc4295955
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-22 14:22:15 -0500

    cambiamos la desccipccion y los generos de las peliculas

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -40,9 +40,80 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
         slivers: [
           _CustomSliverAppBar(
             movie: movie,
-          )
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            childCount: 1,
+            (context, index) => _MovieDescription(movie: movie),
+          ))
         ],
       ),
+    );
+  }
+}
+
+class _MovieDescription extends StatelessWidget {
+  final Movie movie;
+  const _MovieDescription({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final TextStyle = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: TextStyle.titleLarge,
+                    ),
+                    Text(movie.overview)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Chip(
+                      label: Text(gender),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+        //TODO: Actores
+        const SizedBox(
+          height: 100,
+        )
+      ],
     );
   }
 }
