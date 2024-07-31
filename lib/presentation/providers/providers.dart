ebd1a8144f61d636d86a07a8f004c8e7fb6c2aec
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-31 16:15:16 -0500

    cargar las peliculas

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -7,3 +7,4 @@ export 'actors/actor_by_movie_provider.dart';
 export 'actors/actor_repository_provider.dart';
 export 'search/search_movie_provider.dart';
 export 'storage/local_storage_repository_provider.dart';
+export 'storage/favorite_movies_provider.dart';
---
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -0,0 +1,32 @@
+import 'package:cinemacuenca/domain/entities/movie.dart';
+import 'package:cinemacuenca/domain/repositories/local_storage_repository.dart';
+import 'package:cinemacuenca/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final favoriteMoviesProvider =
+    StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>(
+  (ref) {
+    final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+    return StorageMoviesNotifier(
+        localStorageRepository: localStorageRepository);
+  },
+);
+
+class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+
+  StorageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final movies = await localStorageRepository.loadMovies(offset: page * 10);
+    page++;
+
+    final tempMovieMap = <int, Movie>{};
+    for (final movie in movies) {
+      tempMovieMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMovieMap};
+  }
+}
