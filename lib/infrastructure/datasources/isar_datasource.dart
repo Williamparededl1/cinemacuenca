87a0816b2b2721372a913ee03b0f49df9e81f84c
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-31 14:34:26 -0500

    configaracion de la base base en el datasource e instlalcion de path provider

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,7 +1,24 @@
 import 'package:cinemacuenca/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemacuenca/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema],
+          inspector: true, directory: dir.path);
+    }
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMovieFavorite(int movieId) {
     throw UnimplementedError();
