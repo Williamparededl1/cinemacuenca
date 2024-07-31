d0bbab9cf3900da5d72dfdc04864200a69fc3459
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-31 10:36:32 -0500

    peticiones para almacenar la busqueda

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -74,6 +74,7 @@ class MoviedbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> searchMovies(String query) async {
+    if (query.isEmpty) return [];
     final response =
         await dio.get('/search/movie', queryParameters: {'query': query});
 
