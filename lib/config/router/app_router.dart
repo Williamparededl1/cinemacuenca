5962b28a5c32db76a240aa0df154b9365bb26796
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-31 12:27:40 -0500

    cambios en el home screen jeje

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,11 +1,16 @@
 import 'package:cinemacuenca/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
+final appRouter = GoRouter(initialLocation: '/home/0', routes: [
   GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final pageIndex = state.pathParameters['page'] ?? '0';
+        return HomeScreen(
+          pageIndex: int.parse(pageIndex),
+        );
+      },
       routes: [
         GoRoute(
           path: 'movie/:id',
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,15 +1,25 @@
+import 'package:cinemacuenca/presentation/views/views.dart';
 import 'package:cinemacuenca/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home_screen';
-  const HomeScreen({super.key});
+  final int pageIndex;
+
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewsRouters = const <Widget>[
+    HomeView(),
+    SizedBox(),
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: const Center(
-        child: Text('casi casi'),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewsRouters,
       ),
       bottomNavigationBar: CustomBottomNavigation(),
     );
