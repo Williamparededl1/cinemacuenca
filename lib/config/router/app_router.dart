73104f7271b62d4a4ef041eb081481e081d52c0d
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-31 11:43:46 -0500

    implementaciond e gorouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,20 +3,49 @@ import 'package:cinemacuenca/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(
-            childView: FavoritesView(),
-          ),
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+            ]),
+        GoRoute(
+          path: '/favorites',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
+            return const FavoritesView();
           },
         ),
-      ]),
+      ])
+
+//Rutas padre / Hijo
+//  GoRoute(
+//       path: '/',
+//       name: HomeScreen.name,
+//       builder: (context, state) => const HomeScreen(
+//             childView: FavoritesView(),
+//           ),
+//       routes: [
+  // GoRoute(
+  //   path: 'movie/:id',
+  //   name: MovieScreen.name,
+  //   builder: (context, state) {
+  //     final movieId = state.pathParameters['id'] ?? 'no-id';
+  //     return MovieScreen(movieId: movieId);
+  //   },
+  // ),
+//       ]),
 ]);
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outlined), label: 'Categorias'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
-    ]);
+    return BottomNavigationBar(
+        elevation: 0,
+        currentIndex: getCurrentIndex(context),
+        onTap: (value) => onItemTapped(context, value),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outlined), label: 'Categorias'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        ]);
   }
 }
