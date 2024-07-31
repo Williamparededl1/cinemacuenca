a12e32406326fda0a4a492987b73485eb90b22a9
Williamparededl1 <wparedesl1995@gmail.com>
2024-07-31 16:44:16 -0500

    mostrar los titulos de la base

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,35 @@
+import 'package:cinemacuenca/domain/entities/movie.dart';
+import 'package:cinemacuenca/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final mapMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('favoritos vista'),
-      ),
-      body: const Center(
-        child: Text('Tus Peliculas Favoritas'),
-      ),
-    );
+        body: ListView.builder(
+      itemCount: mapMovies.length,
+      itemBuilder: (context, index) {
+        final movie = mapMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    ));
   }
 }
